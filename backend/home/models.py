4cfc20ea761605d8c6f3ce8d9548d71f828be28d
Crowdbotics <engineering@crowdbotics.com>
2022-08-18 14:37:44 +0000

    Update models

--- backend/home/models.py
+++ backend/home/models.py
@@ -1,1 +1,11 @@
+from django.conf import settings
 from django.db import models
+
+
+class Product(models.Model):
+    "Generated Model"
+    name = models.CharField(
+        max_length=100,
+    )
+    description = models.TextField()
+    price = models.IntegerField()
--- backend/mobileapp_test_0817_29355/wsgi.py
+++ backend/mobileapp_test_0817_29355/wsgi.py
@@ -11,6 +11,6 @@ import os
 
 from django.core.wsgi import get_wsgi_application
 
-os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'mobileapp_test_0817_29355.settings')
+os.environ.setdefault("DJANGO_SETTINGS_MODULE", "mobileapp_test_0817_29355.settings")
 
 application = get_wsgi_application()
